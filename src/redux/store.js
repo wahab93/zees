df34158fd3206885d166003336065f7a8157ec0e
Abdul wahab <shahggreat09@gmail.com>
2023-08-15 14:23:30 +0500

    refresh changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,20 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from 'redux-persist/es/storage/session'
 
-const store = createStore(rootReducer)
+
+const persistConfig = {
+    key: 'main-root',
+    storage: storageSession,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, applyMiddleware())
+
+const persistor = persistStore(store)
+
+export { persistor }
 
 export default store;
